0714d0d74d2d6c3b4a22d5bc5b53fb0d25c2d7d1
jihye032 <jihae032@naver.com>
2021-07-11 20:37:42 +0900

    BOJ 10870

--- recursive_function/10870.py
+++ recursive_function/10870.py
@@ -1,2 +1,10 @@
 # 10870번 피보나치수 5
 # https://www.acmicpc.net/problem/10870
+
+def fibonacci(n):
+    if n<=1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+n = int(input())
+print(fibonacci(n))
