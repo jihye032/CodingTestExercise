1e0eb01c93442f8a757510ee648dd372fece4410
jihye032 <jihae032@naver.com>
2021-07-12 22:33:25 +0900

    BOJ 10870

--- recursive_function/10870.py
+++ recursive_function/10870.py
@@ -1,10 +1,19 @@
 # 10870번 피보나치수 5
 # https://www.acmicpc.net/problem/10870
 
-def fibonacci(n):
-    if n<=1:
-        return n
-    return fibonacci(n-1) + fibonacci(n-2)
+def star(x, y):
+    for n in range(x):
+        for m in range(y):
+            i = int(n/3)
+            if i==1:
+                if n%3==1 and m%3==1:
+                    print(" ")
+                else:
+                    print("*")
+            else :
+                star(i, i)
+        print("\n")
+
 
 n = int(input())
-print(fibonacci(n))
+star(n, n)
